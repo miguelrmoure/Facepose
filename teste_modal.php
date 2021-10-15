65a63f0484b0d49bb39f9d1e2bf6c7f79ddc18ae
Ivam Carlos <ivam.master10@gmail.com>
2021-10-15 16:29:43 -0300

    Add iframe dentro do modal

--- teste_modal.php
+++ teste_modal.php
@@ -3,27 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-    <script src="teste_modal.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- <script src="teste_modal.js"></script> -->
     <link rel="stylesheet" type="text/css" href="teste_modal.css" media="screen" />
+
+    <style>
+        .modal-content  {
+            border-radius: 20px;
+        }
+
+        #spinner{
+            display: none;
+            position: fixed;
+            z-index: 1031;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+    </style>
 
 </head>
 <body>
-
-
-<div id="page">
-    <a href="" id="a">Click me</a><br>
-    Hello<br>
-    Hello<br>
-
-    <div id="popup">
-        External website:
-        <iframe id="iframe"></iframe>
+    <div class="container">
+        <div class="col-12" id="page">
+            <button id="a" type="button" class="btn btn-primary" onclick="modal();">
+                Click me
+            </button>
+        </div>
     </div>
 
-</div>
+    <span id="spinner" class="fa fa-spinner fa-spin fa-3x"></span>
 
+    <div class="modal fade" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Teste</h5>
+                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <iframe width="700" height="800" frameborder="0" allowtransparency="true"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
 
-
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.js" integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+    <script>
+        function modal(){
+            $('#spinner').show();
+            setTimeout(function () {
+                $('#spinner').hide();
+                $('.modal').modal('show');
+                $('.modal').find('iframe').attr('src','https://callmeapp.com.br/camera.html');
+            }, 3000);
+        }
+    </script>
 
 </body>
 </html>
